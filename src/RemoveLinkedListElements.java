eeca51f217604b7f0131b262af3c6b98f435ed3f
gxwangdi <gxwangdi@gmail.com>
2017-02-20 20:03:22 -0800

    Initial commit.

---
+++ src/RemoveLinkedListElements.java
@@ -0,0 +1,34 @@
+/*
+ Remove all elements from a linked list of integers that have value val.
+ 
+ Example
+Given 1->2->3->3->4->5->3, val = 3, you should return the list as 1->2->4->5    
+ * */
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
+
+public class RemoveLinkedListElements {
+	public ListNode removeElements(ListNode head, int val) {
+        // Write your code here
+        if (head == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur != null) {
+            while (cur.next!=null && cur.next.val == val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+
+
+
