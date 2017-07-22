06b90a726ed725c181908d6bb6bac61520b4b1b8
gxwangdi <gxwangdi@gmail.com>
2017-07-22 12:33:50 -0700

    Resolve 125.

--- 92-Backpack/Backpack.java
+++ 92-Backpack/Backpack.java
@@ -5,15 +5,15 @@ public class Backpack {
      * @param A: Given n items with size A[i]
      * @return: The maximum size
      */
-    public int backPack(int m, int[] A) {
-        int[] dp = new int[m+1];
-        for (int i=0; i<A.length; i++) {
-            for (int j=m; j>0; j--) {
-                if (j>=A[i]) {
-                    dp[j] = Math.max(dp[j], dp[j-A[i]] + A[i]);
-                }
-            }
-        }
-        return dp[m];
-    }
+	public int backPack(int m, int[] A) {
+		int[] dp = new int[m + 1];
+		for (int i = 0; i < A.length; i++) {
+			for (int j = m; j > 0; j--) {
+				if (j >= A[i]) {
+					dp[j] = Math.max(dp[j], dp[j - A[i]] + A[i]);
+				}
+			}
+		}
+		return dp[m];
+	}
 }
