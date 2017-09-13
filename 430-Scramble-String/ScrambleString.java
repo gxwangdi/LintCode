ddc1908905580aa530ad67d324898e17003dfc4d
Di Wang <gxwangdi@gmail.com>
2017-09-12 23:15:08 -0700

    Rename dp array.

--- 430-Scramble-String/ScrambleString.java
+++ 430-Scramble-String/ScrambleString.java
@@ -14,28 +14,28 @@ public class ScrambleString {
 		if (len == 0)
 			return true;
 
-		boolean[][][] d = new boolean[len + 1][len][len];
+		boolean[][][] dp = new boolean[len + 1][len][len];
 		for (int i = 0; i < len; i++)
 			for (int j = 0; j < len; j++)
 				if (s1.charAt(i) == s2.charAt(j))
-					d[1][i][j] = true;
+					dp[1][i][j] = true;
 				else
-					d[1][i][j] = false;
+					dp[1][i][j] = false;
 
 		for (int k = 2; k <= len; k++)
 			for (int i = 0; i <= len - k; i++)
 				for (int j = 0; j <= len - k; j++)
 					for (int l = 1; l <= (k - 1); l++) {
-						d[k][i][j] = false;
-						if (d[l][i][j] && d[k - l][i + l][j + l]) {
-							d[k][i][j] = true;
+						dp[k][i][j] = false;
+						if (dp[l][i][j] && dp[k - l][i + l][j + l]) {
+							dp[k][i][j] = true;
 							break;
-						} else if (d[l][i][j + k - l] && d[k - l][i + l][j]) {
-							d[k][i][j] = true;
+						} else if (dp[l][i][j + k - l] && dp[k - l][i + l][j]) {
+							dp[k][i][j] = true;
 							break;
 						}
 					}
 
-		return d[len][0][0];
+		return dp[len][0][0];
 	}
 }
